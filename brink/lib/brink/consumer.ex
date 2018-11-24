ff1d878038f0f8a992f261ad7e8d1fd82a080290
Ramon Snir <r@mon.co.il>
2018-11-24 09:28:06 -0500

    XREAD support in Brink.Consumer

--- brink/lib/brink/consumer.ex
+++ brink/lib/brink/consumer.ex
@@ -16,7 +16,7 @@ defmodule Brink.Consumer do
   #   doesn't really allow for pending messages to be created because it's
   #   using NOACK. We should find a way to track processing of messages to
   #   XACK them properly only once they've been processed.
-  # - The code should support a non-group XREAD mode for simpler usage.
+  # - The code should support multiple streams being tracked individually.
 
   def start_link(options \\ []) do
     GenStage.start_link(__MODULE__, options, name: Keyword.get(options, :name, __MODULE__))
@@ -34,30 +34,60 @@ defmodule Brink.Consumer do
           client
       end
 
+    mode = Keyword.get(options, :mode, :single)
+
     with {:ok, stream} <- Keyword.fetch(options, :stream),
-         {:ok, group} <- Keyword.fetch(options, :group),
-         {:ok, consumer} <- Keyword.fetch(options, :consumer) do
+         {:ok, mode_state} <- parse_mode_options(mode, options) do
       state = %{
         client: redis_client,
         stream: stream,
-        group: group,
-        consumer: consumer,
+        mode: mode,
         demand: 0,
-        next_id: "0",
         poll_interval: Keyword.get(options, :poll_interval, 100)
       }
 
-      dispatcher = Keyword.get(options, :dispatcher, nil)
+      producer_options =
+        case Keyword.fetch(options, :dispatcher) do
+          {:ok, dispatcher} ->
+            [dispatcher: dispatcher]
 
-      if dispatcher do
-        {:producer, state, dispatcher: dispatcher}
-      else
-        {:producer, state}
-      end
+          _ ->
+            []
+        end
+
+      {:producer, Map.merge(state, mode_state), producer_options}
     else
-      _ -> {:stop, "Missing arguments"}
+      _ -> {:stop, "invalid arguments"}
     end
   end
+
+  defp parse_mode_options(:single, options) do
+    with {:ok, start_id} <- Keyword.fetch(options, :start_id) do
+      {:ok,
+       %{
+         next_id: start_id,
+         initial_block_timeout: Keyword.get(options, :initial_block_timeout, 1_000)
+       }}
+    else
+      _ -> {:error, :badarg}
+    end
+  end
+
+  defp parse_mode_options(:group, options) do
+    with {:ok, group} <- Keyword.fetch(options, :group),
+         {:ok, consumer} <- Keyword.fetch(options, :consumer) do
+      {:ok,
+       %{
+         group: group,
+         consumer: consumer,
+         next_id: "0"
+       }}
+    else
+      _ -> {:error, :badarg}
+    end
+  end
+
+  defp parse_mode_options(_mode, _options), do: {:error, :badarg}
 
   def handle_info(:read_from_stream, state) do
     read_from_stream(state)
@@ -66,8 +96,8 @@ defmodule Brink.Consumer do
   # ignoring incoming messages to clear mailbox
   def handle_info(_, state), do: {:noreply, [], state}
 
-  def handle_demand(incoming_demand, %{demand: demand} = state) do
-    new_state = %{state | demand: demand + incoming_demand}
+  def handle_demand(incoming_demand, state) do
+    new_state = %{state | demand: state.demand + incoming_demand}
 
     read_from_stream(new_state)
   end
@@ -75,11 +105,11 @@ defmodule Brink.Consumer do
   defp read_from_stream(%{demand: 0} = state), do: {:noreply, [], state}
 
   defp read_from_stream(%{stream: stream} = state) do
-    read_command = build_xreadgroup(state)
+    read_command = build_xread(state)
 
     case Redix.command(state.client, read_command) do
       {:ok, [[^stream, [_ | _] = events]]} ->
-        next_id = pick_next_id(state.next_id, events)
+        next_id = pick_next_id(state, events)
 
         new_state = %{state | demand: state.demand - length(events), next_id: next_id}
 
@@ -93,8 +123,7 @@ defmodule Brink.Consumer do
 
       {:ok, _} ->
         poll_stream(state.poll_interval)
-        # If no events were found, there are definitely no pending messages.
-        new_state = %{state | next_id: pick_next_id(state.next_id, [])}
+        new_state = %{state | next_id: pick_next_id(state, [])}
         {:noreply, [], new_state}
 
       {:error, err} ->
@@ -106,7 +135,31 @@ defmodule Brink.Consumer do
     Process.send_after(self(), :read_from_stream, interval)
   end
 
-  defp build_xreadgroup(state) do
+  defp build_xread(%{mode: :single, next_id: "$"} = state) do
+    [
+      "XREAD",
+      "BLOCK",
+      state.initial_block_timeout,
+      "COUNT",
+      state.demand,
+      "STREAMS",
+      state.stream,
+      "$"
+    ]
+  end
+
+  defp build_xread(%{mode: :single} = state) do
+    [
+      "XREAD",
+      "COUNT",
+      state.demand,
+      "STREAMS",
+      state.stream,
+      state.next_id
+    ]
+  end
+
+  defp build_xread(%{mode: :group} = state) do
     [
       "XREADGROUP",
       "GROUP",
@@ -121,9 +174,18 @@ defmodule Brink.Consumer do
     ]
   end
 
-  defp pick_next_id(_previous_next_id, []), do: ">"
+  defp pick_next_id(%{mode: :single, next_id: next_id}, []), do: next_id
 
-  defp pick_next_id(previous_next_id, events) do
+  defp pick_next_id(%{mode: :single}, events) do
+    events
+    |> Enum.map(&List.first/1)
+    |> List.last()
+  end
+
+  # If no events were found (history scanning or not), there are definitely no pending messages.
+  defp pick_next_id(%{mode: :group}, []), do: ">"
+
+  defp pick_next_id(%{mode: :group, next_id: previous_next_id}, events) do
     if previous_next_id == ">" do
       ">"
     else
