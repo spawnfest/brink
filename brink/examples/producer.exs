e9335f520184c9c0c60a3d61dc4be9d57ae76c73
Ramon Snir <r@mon.co.il>
2018-11-23 23:02:30 -0500

    maxlen as protection

--- brink/examples/producer.exs
+++ brink/examples/producer.exs
@@ -1,7 +1,7 @@
 Flow.from_enumerable(1..10_000)
 |> Flow.partition()
 |> Flow.map(&%{v: &1, w: &1 * 2})
-|> Flow.into_specs([{{Brink.Producer, [redis_uri: "redis://127.0.0.1:6379"]}, []}])
+|> Flow.into_specs([{{Brink.Producer, [maxlen: 20_000]}, []}])
 
 # Surely there's a better way?
 Process.sleep(10_000)
